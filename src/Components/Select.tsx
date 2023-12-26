5955fd46e4f093b4565366870ec3238a7cf6f69a
slxng1758 <148385226+slxng1758@users.noreply.github.com>
2023-12-26 15:17:38 -0500

    more commits

--- src/Components/Select.tsx
+++ src/Components/Select.tsx
@@ -1,43 +1,65 @@
-function Select(){
-    return(
-        <div className="row m-3" style={{ backgroundColor: "#F0F0F0"}}>
-            <h1>Select a hospital location to view a detailed layout</h1>
-  <div className="col-sm-4">
-  <div className="card-body ml-3">
-    <img
-    src="./src/assets/birchmount.png"
-    className="d-block w-100 img-fluid"
-    alt="..."
-  />
-     <p className="card-text">3030 Birchmount Rd, Scarborough, ON M1W 3W3, Canada</p>
-    <button type="button" className="btn btn-primary btn-lg " style={{backgroundColor: "#0078B6"}}>Birchmount Hospital</button>
-</div>
-  </div>
-  <div className="col-sm-4">
-  <div className="card-body ml-3">
-    <img
-    src="./src/assets/centenary.png"
-    className="d-block w-100 img-fluid"
-    alt="..."
-  />
-     <p className="card-text">2867 Ellesmere Rd, Scarborough, ON M1E 4B9, Canada</p>
-    <button type="button" className="btn btn-primary btn-lg "style={{backgroundColor: "#0078B6"}}>Centenary Hospital</button>
-</div>
-
-  </div>
-  <div className="col-sm-4">
-  <div className="card-body ml-3">
-    <img
-    src="./src/assets/general.png"
-    className="d-block w-100 img-fluid"
-    alt="..."
-  />
-     <p className="card-text">3050 Lawrence Ave E, Scarborough, ON M1P 2V5, Canada</p>
-    <button type="button" className="btn btn-primary btn-lg " style={{backgroundColor: "#0078B6"}}>General Hospital</button>
-</div>
-</div>
-</div>
-    );
-
+function Select() {
+  return (
+    <div className="row m-5  border-5 border-bottom border-start border-info" style={{ backgroundColor: "#F0F0F0" }}>
+      <h1>Select a hospital location to view a detailed layout</h1>
+      <div className="col-sm-4">
+        <div className="card-body ml-3">
+          <img
+            src="./src/assets/birchmount.png"
+            className="d-block w-100 img-fluid"
+            alt="..."
+          />
+          <p className="card-text">
+            3030 Birchmount Rd, Scarborough, ON M1W 3W3, Canada
+          </p>
+          <button
+            type="button"
+            className="btn btn-primary btn-lg "
+            style={{ backgroundColor: "#0078B6" }}
+          >
+            Birchmount Hospital
+          </button>
+        </div>
+      </div>
+      <div className="col-sm-4">
+        <div className="card-body ml-3">
+          <img
+            src="./src/assets/centenary.png"
+            className="d-block w-100 img-fluid"
+            alt="..."
+          />
+          <p className="card-text">
+            2867 Ellesmere Rd, Scarborough, ON M1E 4B9, Canada
+          </p>
+          <button
+            type="button"
+            className="btn btn-primary btn-lg "
+            style={{ backgroundColor: "#0078B6" }}
+          >
+            Centenary Hospital
+          </button>
+        </div>
+      </div>
+      <div className="col-sm-4">
+        <div className="card-body ml-3">
+          <img
+            src="./src/assets/general.png"
+            className="d-block w-100 img-fluid"
+            alt="..."
+          />
+          <p className="card-text">
+            3050 Lawrence Ave E, Scarborough, ON M1P 2V5, Canada
+          </p>
+          <button
+            type="button"
+            className="btn btn-primary btn-lg "
+            style={{ backgroundColor: "#0078B6" }}
+          >
+            General Hospital
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Select;
--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -4,7 +4,7 @@ import NavBar from "../Components/NavBar";
 import LatestCarousel from "../Components/LatestCarousel";
 import Footer from "../Components/Footer";
 import Select from "../Components/Select";
-import Reminder from "../Components/reminder";
+import Reminder from "../Components/Reminder";
 
 function Home() {
   return (
